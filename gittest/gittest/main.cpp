f360a46abb8ea5028cc0e3d57bad88fd9a5cace4
18019616097 <614760358@qq.com>
2017-10-25 14:40:25 +0800

    创建一个空的c++项目

---
+++ gittest/gittest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  gittest
+//
+//  Created by student on 17/10/25.
+//  Copyright © 2017年 zhang hao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
